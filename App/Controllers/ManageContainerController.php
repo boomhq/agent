fa12f395d00c3668d08b1a2f89625736c636a909
boom <nicos.bentz@gmail.com>
2017-09-14 15:27:33 +0200

    Add controllers

---
+++ App/Controllers/ManageContainerController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Services\Docker\DockerService;
+use App\Services\Docker\Network\NetworkService;
+use App\Services\Docker\Provisioning\ProvisioningService;
+
+class ManageContainerController extends Controller
+{
+
+    private $dockerService;
+    private $provisioningService;
+
+
+    public function __construct()
+    {
+        $this->dockerService = new DockerService();
+        $this->provisioningService = new ProvisioningService();
+    }
+
+
+    public function buildMinecraftContainer(
+        string $portExposedFromHost,
+        string $nameOfContainer = null,
+        string $portExposedFromContainer = '25565',
+        string $versionMinecraft = 'SNAPSHOT'
+    ) {
+
+        //check port is Good and not used
+
+        if ($portExposedFromHost < 1024 || $portExposedFromHost > 65536) {
+            return "Impossible, le port selectionné ne fait pas partie d'une plage de port autorisés";
+        }
+
+        $networkService = new NetworkService();
+        $allPortsExposed = $networkService->getAllPortsHostExposed();
+
+        if (in_array($portExposedFromHost, $allPortsExposed)) {
+            return "Le port selectionné est utilisé";
+        }
+
+
+        //prepare Env Array
+        $envVariable = [
+            'EULA' => 'TRUE',
+            'VERSION' => $versionMinecraft,
+            'MIN_RAM' => '256M',
+            'MAX_RAM' => '256M',
+        ];
+
+        $provisioningService = new ProvisioningService();
+        $newContainer = $provisioningService->createContainer(
+            $envVariable,
+            $portExposedFromHost,
+            $portExposedFromContainer
+        );
+
+        return $newContainer;
+    }
+}
--- bin/main.php
+++ bin/main.php
@@ -1,7 +1,39 @@
 <?php
 
-require_once __DIR__ . '/../App/Boot/bootstrap.php';
+require_once __DIR__.'/../App/Boot/bootstrap.php';
 
-$app = new \App\Services\Docker\DockerService();
-$r = $app->findAllContainers();
-print_r($r);
+$x = new \App\Controllers\ManageContainerController();
+
+$z = $x->buildMinecraftContainer('25565');
+
+
+print_r($z);
+
+$docker = new \Docker\Docker();
+$containerManager = $docker->getContainerManager();
+
+$attachStream = $containerManager->attach(
+    $z->getId(),
+    [
+        'stream' => true,
+        'stdin' => true,
+        'stdout' => true,
+        'stderr' => true,
+    ]
+);
+
+$containerManager->start($z->getId());
+
+
+$attachStream->onStdout(
+    function ($stdout) {
+        echo $stdout;
+    }
+);
+$attachStream->onStderr(
+    function ($stderr) {
+        echo $stderr;
+    }
+);
+
+$attachStream->wait();
