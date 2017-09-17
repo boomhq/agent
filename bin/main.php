1c5204535cf3a6aec0edc4b98c3fc99d7b8091f2
boom <nicos.bentz@gmail.com>
2017-09-17 16:29:33 +0200

    proper

--- bin/main.php
+++ bin/main.php
@@ -1,9 +1,3 @@
 <?php
 
 require_once __DIR__.'/../App/bootstrap.php';
-
-$test = new \App\Services\Docker\Console\ConsoleService();
-
-$x = $test->executeCommandInConsoleAttach( 'mc',"help");
-
-print_r($x);
